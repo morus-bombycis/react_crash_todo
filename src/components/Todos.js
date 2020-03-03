91235ddbf598add3e45a9d9e9a6743b9209168df
morus-bombycis <christopher.perez.ubiqum@gmail.com>
2020-03-03 14:30:01 +0100

    Completed

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,16 +3,20 @@ import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component {
+
     render() {
-        return this.todos.props.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} />
+        return this.props.todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} markComplete=
+                {this.props.markComplete} delTodo={this.props.delTodo} />
         ));
     }
 }
 
 //Proptypes
 Todos.propTypes = {
-    todos: PropTypes.array.isRequired
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired,
 }
 
 export default Todos;
